dd2438527d79cb3e5a2caac807a3815769e4b2da
ramakrushnapradhan <64239718+ramakrushnapradhan@users.noreply.github.com>
2021-07-15 19:52:14 +0530

    Create head_recurrsion.java

---
+++ 1.Recurssions/head_recurrsion.java
@@ -0,0 +1,21 @@
+//Head Recursion
+public class head_recursion
+ {
+     static void fun(int n)
+      {
+        if(n>0)
+	         {
+             fun(n-1);
+	           System.out.println(n);
+	         }
+	     }
+      public static void main(String[] args)
+       {
+         int x=3;
+	       fun(x);
+       }
+ }
+Output :
+         1
+         2
+         3
